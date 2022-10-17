4108d551c30c79069210df1606139312331b74f2
joseforeromt <miguelforero1993@gmail.com>
2022-10-17 14:17:28 -0500

    first commit​

---
+++ js/funcionesReportes.js
@@ -0,0 +1,32 @@
+function getStatus(){
+    $.ajax({
+        url:"http://152.67.224.3/api/Reservation/report-status",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            pintarStatus(respuesta);
+        }
+    });
+
+}
+
+function pintarStatus(respuesta){
+    let myTable="<table>";
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta.completed+"</td>";
+        myTable+="<td>"+respuesta.cancelled+"</td>";
+        myTable+="</tr>";
+    
+    myTable+="</table>";
+    $("#resultado1").html(myTable);
+
+}
+
+function getFechas(){
+
+
+}
+
+function getClientes(){
+
+}
